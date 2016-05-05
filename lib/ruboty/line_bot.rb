992b226c2e2815daae40a4fbd89ca5622f974441
meganemura <meganemura@users.noreply.github.com>
2016-05-05 10:06:51 +0900

    bundle gem ruboty-line_bot

---
+++ lib/ruboty/line_bot.rb
@@ -0,0 +1,7 @@
+require "ruboty/line_bot/version"
+
+module Ruboty
+  module LineBot
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/line_bot/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module LineBot
+    VERSION = "0.1.0"
+  end
+end
