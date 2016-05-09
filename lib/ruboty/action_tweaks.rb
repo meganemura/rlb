f492b6ca79a9cdd9753228e9207727b557d3b2cd
meganemura <meganemura@users.noreply.github.com>
2016-05-09 23:39:34 +0900

    Respond all messages without robot name if user specifies envvar

---
+++ lib/ruboty/action_tweaks.rb
@@ -0,0 +1,8 @@
+module Ruboty
+  class Action
+    # return true to respond every message without robot name.
+    def all?
+      true
+    end
+  end
+end
--- lib/ruboty/line_bot.rb
+++ lib/ruboty/line_bot.rb
@@ -1,2 +1,5 @@
 require "ruboty/line_bot/version"
 require "ruboty/adapters/line_bot"
+if ENV['RESPOND_TO_ALL'] == '1'
+  require "ruboty/action_tweaks"
+end
